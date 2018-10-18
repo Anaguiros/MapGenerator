441ec4cedbffe2a780065a60dd13a2b9336b4588
Matthieu Lenogue <matthieu.lenogue@gmail.com>
2018-10-18 16:31:19 +0200

    Generation Island

--- heightmap.js
+++ heightmap.js
@@ -2,6 +2,47 @@ var heightmap = new Array();
 
 function generateHeights() {
     for (let i = 0; i < sites.length; i++) {
-        heightmap.splice( i, 0, Math.random());
+        heightmap.splice( i, 0, 0);
     }
+}
+
+function add(polygonStartID, type){
+    var explorationQueue = new Array(),
+    exploredPolygon = new Array(sites.length-1);
+
+    for (let i = 0; i < exploredPolygon.length; i++) {
+        exploredPolygon[i] = false;
+    }
+
+    var height = highInput.valueAsNumber,
+    radius = radiusInput.valueAsNumber,
+    sharpness = sharpnessInput.valueAsNumber;
+
+    heightmap[polygonStartID] += height;
+    exploredPolygon[polygonStartID] = true;
+    explorationQueue.push(polygonStartID);
+    for (let i = 0; i < explorationQueue.length && height > 0.01; i++) {
+        if(type == "island"){
+            height = heightmap[explorationQueue[i]] * radius;
+        } else {
+            height *= radius;
+        }
+        for (let neighborID of delaunay.neighbors(explorationQueue[i])) {
+            if(!exploredPolygon[neighborID]){
+                var noise = Math.random()*sharpness + 1.1 - sharpness;
+                if(sharpness == 0){
+                    noise = 1;
+                }
+                heightmap[neighborID] += height * noise;
+                if(heightmap[neighborID] > 1){
+                    heightmap[neighborID] = 1;
+                }
+                exploredPolygon[neighborID] = true;
+                explorationQueue.push(neighborID);
+            }
+        }
+    }
+
+    //affichage données mise à jour
+    show();
 }
--- utils.js
+++ utils.js
@@ -1,9 +1,12 @@
-var width = 960,
-    height = 500,
-    numberPoints = 10;
+var width_canvas = 960,
+    height_canvas = 500,
+    numberPoints = 8000;
 
-var canvas = d3.select("canvas").attr("width", width).attr("height", height);
-canvas.on("touchmove mousemove", moved);
+var canvas = d3.select("canvas")
+    .attr("width", width_canvas)
+    .attr("height", height_canvas)
+    .on("touchmove mousemove", moved)
+    .on("click", clicked);
 const context = document.getElementById('chart').getContext('2d');
 var color = d3.scaleSequential(d3.interpolateSpectral);
 
@@ -14,15 +17,11 @@ var voronoi;
 
 generate();
 
-console.log(voronoi);
-console.log(delaunay);
-
 function generate(){
-    console.log("generate !");
-    sites = Array.from({length: numberPoints}, () => [Math.random() * width, Math.random() * height]);
+    sites = Array.from({length: numberPoints}, () => [Math.random() * width_canvas, Math.random() * height_canvas]);
 
     delaunay = new d3.Delaunay.from(sites);
-    voronoi = delaunay.voronoi([0.5, 0.5, width - 0.5, height - 0.5]);
+    voronoi = delaunay.voronoi([0.5, 0.5, width_canvas - 0.5, height_canvas - 0.5]);
     relax();
 }
 
@@ -34,7 +33,7 @@ function relax(){
 
     sites=relaxedSites
     delaunay = new d3.Delaunay.from(sites);
-    voronoi = delaunay.voronoi([0.5, 0.5, width - 0.5, height - 0.5]);
+    voronoi = delaunay.voronoi([0.5, 0.5, width_canvas - 0.5, height_canvas - 0.5]);
 
     generateHeights();
 
@@ -76,14 +75,30 @@ function showVoronoi(){
 }
 
 function clearCanvas(){
-    context.clearRect(0,0,width,height);
+    context.clearRect(0,0,width_canvas,height_canvas);
 }
 
 function moved(){
     var point = d3.mouse(this),
-    nearestId = delaunay.find(point[0], point[1]),
+    nearestId = delaunay.find(point[0], point[1]);
 
     d3.select("#cell").text(nearestId);
     d3.select("#high").text(heightmap[nearestId])
+}
+
+function clicked(){
+    var point = d3.mouse(this),
+    nearestId = delaunay.find(point[0], point[1]);
+
+    if(heightmap.findIndex(function(element) {return element > 0}) != -1){
+        //premier ajout
+        add(nearestId, 'island');
+        highOutput.value = 0.2;
+        radiusInput.value = 0.99;
+        radiusOutput.value = 0.99;
+    } else {
+        add(nearestId, 'hill');
+        highInput.value = Math.random() * 0.4 + 0.1;
+    }
 }
 
